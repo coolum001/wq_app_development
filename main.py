af570492a22a201997f1382842731f831d8fc4e9
Don Cameron <donrcameron@gmail.com>
2024-08-21 09:16:32 +0000

    Change poty to 8050

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ callbacks(app)
 app.layout = layout
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(host='0.0.0.0', port=8050, debug=True)
